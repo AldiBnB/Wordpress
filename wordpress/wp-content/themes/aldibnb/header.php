d975c9c292dc6cf174e6131fe71c760f85718c2a
eva-vrg <evaverger08@gmail.com>
2022-03-06 05:35:30 +0100

    test homepage

--- wordpress/wp-content/themes/aldibnb/header.php
+++ wordpress/wp-content/themes/aldibnb/header.php
@@ -1,30 +1,25 @@
-<!doctype html>
-<html lang="fr">
-
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <?php wp_head(); ?>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Agencia</title>
+  <link rel="stylesheet" href="style.e2e1a33c.css">
+  <script src="main.ef23f591.js" defer=""></script>
 </head>
-
-<body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="/">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <?php wp_nav_menu([
-                    'theme_location' => 'header',
-                    'container' => false,
-                    'menu_class' => "navbar-nav me-auto mb-2 mb-lg-0"
-                ]); ?>
-
-                <?php get_search_form(); ?>
-
-            </div>
-        </div>
-    </nav>
-    <div class='container'>
+<body class="home">
+<header class="nav">
+  <a href="index.html" class="nav__logo" title="Page d'accueil">
+    <img src="logo.5b7fbf6d.svg" alt="">
+  </a>
+  <ul class="nav__menu">
+    <li><a href="#">Acheter</a></li>
+    <li><a href="listing.html" aria-current="page">Louer</a></li>
+    <li><a href="news.html">Actualité</a></li>
+    <li><a href="#">Contact</a></li>
+  </ul>
+  <button class="nav__burger">
+    <span></span>
+  </button>
+</header>
