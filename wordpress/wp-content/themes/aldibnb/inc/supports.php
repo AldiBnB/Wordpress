b377c60887abdf63c3211cfea8f7854791bc94da
eva-vrg <evaverger08@gmail.com>
2022-03-06 10:20:04 +0100

    style

--- wordpress/wp-content/themes/aldibnb/inc/supports.php
+++ wordpress/wp-content/themes/aldibnb/inc/supports.php
@@ -2,6 +2,11 @@
 
 defined('ABSPATH') or die('');
 
-add_action('after_setup_theme'), function (){
-    add_theme_support
-}
+
+add_action('after_setup_theme', function () {
+    add_theme_support('title-tag');
+    add_theme_support('menus');
+    add_theme_support('html5');
+    add_theme_support('post-thumbnails');
+
+});
