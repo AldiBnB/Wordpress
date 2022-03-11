cdad732ce73545c734c3d4e4f2af8a963ac84dff
Hiteazel <khacminhbrandon.vo@gmail.com>
2022-03-11 22:12:43 +0100

    push làààààà

--- wordpress/wp-content/themes/aldibnb/register.php
+++ wordpress/wp-content/themes/aldibnb/register.php
@@ -44,7 +44,7 @@ if (isset($_POST['register'])) {
 <style>
 
 body {
-    height: 100%;
+    height: 100vh;
     background: linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(255,247,211,1) 100%);
     background-position: center;
     background-repeat: no-repeat;
@@ -61,6 +61,8 @@ body {
 .form {
     font-family: "Georgia";
     font-size: 14px;
+    display:flex;
+    flex-direction: column;
     /* position: relative; */
     text-align: center;
     width: 450px;
@@ -68,14 +70,17 @@ body {
     box-sizing: border-box;
     border-radius: 4px;
     background: white;
-    margin-left: auto;
-    margin-right: auto;
-    position: absolute;
-    top: 45%;
+    margin: 100px auto;
+    /* position: absolute; */
+    /* top: 45%;
     left: 50%;
     -ms-transform: translate(-50%, -50%);
-    transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%); */
     box-shadow: 0px 0px 30px 10px rgba(0,0,0,0.4);
+}
+
+.input {
+    margin-bottom: 20px;
 }
 
 </style>
@@ -85,26 +90,23 @@ body {
 
     <?php get_header(); ?>
     <!-- Create a register form -->
-    <form action="<?php echo esc_url(site_url('/register')); ?>" method="post">
-        <div class="form-group">
-            <label for="username">Username</label>
-            <input type="text" name="username" class="form-control" value="<?php echo (!empty($_POST['username'])) ? $_POST['username'] : ''; ?>">
+    <form class="form" action="<?php echo esc_url(site_url('/register')); ?>" method="post">
+        <p class="title">Inscription</p>
+        <div class="input">
+            <input type="text" name="username" placeholder="Username" class="form-control" value="<?php echo (!empty($_POST['username'])) ? $_POST['username'] : ''; ?>">
         </div>
-        <div class="form-group">
-            <label for="email">Email</label>
-            <input type="email" name="email" class="form-control" value="<?php echo (!empty($_POST['email'])) ? $_POST['email'] : ''; ?>">
+        <div class="input">
+            <input type="email" name="email" placeholder="Adresse Email" class="form-control" value="<?php echo (!empty($_POST['email'])) ? $_POST['email'] : ''; ?>">
         </div>
-        <div class="form-group">
-            <label for="password">Mot de passe</label>
-            <input type="password" name="password" class="form-control" value="<?php echo (!empty($_POST['password'])) ? $_POST['password'] : ''; ?>">
+        <div class="input">
+            <input type="password" name="password" placeholder="Mot de passe" class="form-control" value="<?php echo (!empty($_POST['password'])) ? $_POST['password'] : ''; ?>">
         </div>
-        <div class="form-group">
-            <label for="confirm_password">Confirmer le mot de passe</label>
-            <input type="password" name="confirm_password" class="form-control" value="<?php echo (!empty($_POST['confirm_password'])) ? $_POST['confirm_password'] : ''; ?>">
+        <div class="input">
+            <input type="password" name="confirm_password" placeholder="Confirmer le mot de passe" class="form-control" value="<?php echo (!empty($_POST['confirm_password'])) ? $_POST['confirm_password'] : ''; ?>">
         </div>
 
 
-        <div class="form-group">
+        <div class="input">
             <label for="role">Role</label>
             <select name="role" id="role">
                 <!-- role de wordpress -->
@@ -113,7 +115,7 @@ body {
 
             </select>
         </div>
-        <div class="form-group">
+        <div class="input">
             <input type="submit" name="register" class="btn btn-primary" value="Register">
         </div>
     </form>
