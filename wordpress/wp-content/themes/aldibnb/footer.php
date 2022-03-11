201efc286b773d1e19c22ecdac36f2fa653c9d2f
eva-vrg <evaverger08@gmail.com>
2022-03-11 15:41:06 +0100

    grid

--- wordpress/wp-content/themes/aldibnb/footer.php
+++ wordpress/wp-content/themes/aldibnb/footer.php
@@ -1,56 +1,57 @@
 </div>
-    
-<footer class="footer_container">
-  <div class="footer__col1">
-    <div class="footer__title">Pages</div>
-    <ul>
-      <li><a href="#">A propos</a></li>
-      <li><a href="#">Conditions d'utilisations</a></li>
-      <li><a href="#">Politiques de confidentialités</a></li>
-    </ul>
-  </div>
-  <div class="footer__col2">
-    <div class="footer__title">Pages</div>
-    <ul>
-      <li><a href="#">A propos</a></li>
-      <li><a href="#">Conditions d'utilisations</a></li>
-      <li><a href="#">Politiques de confidentialités</a></li>
-    </ul>
-  </div>
-  <div class="footer__col3">
-    <div class="footer__title">Pages</div>
-    <ul>
-      <li><a href="#">A propos</a></li>
-      <li><a href="#">Conditions d'utilisations</a></li>
-      <li><a href="#">Politiques de confidentialités</a></li>
-    </ul>
-  </div>
-  <div class="footer__col4">
-    <div class="footer__title">Pages</div>
-    <ul>
-      <li><a href="#">A propos</a></li>
-      <li><a href="#">Conditions d'utilisations</a></li>
-      <li><a href="#">Politiques de confidentialités</a></li>
-    </ul>
-  </div>
-  <div class="footer__col">
-    <div class="footer__credits">© 2020 Grafikart Real Estate, Inc.</div>
-    <div class="footer__social">
-    <div class="footer__social">
-        <div class="twitter">
-            <img src="https://img.icons8.com/material/30/000000/twitter--v1.png"/>
-        </div class="instagram">
-        <div>
-            <img src="https://img.icons8.com/ios-glyphs/30/000000/facebook.png"/>
+    <div class="container"> 
+      <footer class="footer_container">
+        <div class="footer__col1">
+          <div class="footer__title">Pages</div>
+          <ul>
+            <li><a href="#">A propos</a></li>
+            <li><a href="#">Conditions d'utilisations</a></li>
+            <li><a href="#">Politiques de confidentialités</a></li>
+          </ul>
         </div>
-        <div class="facebook">
-            <img src="https://img.icons8.com/material/30/000000/instagram-new--v1.png"/>
+        <div class="footer__col2">
+          <div class="footer__title">Pages</div>
+          <ul>
+            <li><a href="#">A propos</a></li>
+            <li><a href="#">Conditions d'utilisations</a></li>
+            <li><a href="#">Politiques de confidentialités</a></li>
+          </ul>
         </div>
+        <div class="footer__col3">
+          <div class="footer__title">Pages</div>
+          <ul>
+            <li><a href="#">A propos</a></li>
+            <li><a href="#">Conditions d'utilisations</a></li>
+            <li><a href="#">Politiques de confidentialités</a></li>
+          </ul>
+        </div>
+        <div class="footer__col4">
+          <div class="footer__title">Pages</div>
+          <ul>
+            <li><a href="#">A propos</a></li>
+            <li><a href="#">Conditions d'utilisations</a></li>
+            <li><a href="#">Politiques de confidentialités</a></li>
+          </ul>
+        </div>
+        <div class="footer__col">
+          <div class="footer__credits">© 2020 Grafikart Real Estate, Inc.</div>
+          <div class="footer__social">
+          <div class="footer__social">
+              <div class="twitter">
+                  <img src="https://img.icons8.com/material/30/000000/twitter--v1.png"/>
+              </div class="instagram">
+              <div>
+                  <img src="https://img.icons8.com/ios-glyphs/30/000000/facebook.png"/>
+              </div>
+              <div class="facebook">
+                  <img src="https://img.icons8.com/material/30/000000/instagram-new--v1.png"/>
+              </div>
+          </div>
+        </div>
+      </footer>
     </div>
-  </div>
-</footer>
 
 
-<?php wp_footer() ?>
-</body>
-</html>
+  <?php wp_footer() ?>
+  </body>
+  </html>
